443ee893c6a16e8fd4a0a6907f9e8f0b94461224
CQAllen <allen900625@gmail.com>
2016-03-18 19:31:36 +0800

    first commit
    Angualr notification

---
+++ notify.js
@@ -0,0 +1,24 @@
+/**=========================================================
+ * Module: notify.js
+ * Directive for notify plugin
+ =========================================================*/
+
+app.directive('notify', ["$window", "Notify", function($window, Notify){
+
+  return {
+    restrict: 'A',
+    scope: {
+        options: '=',
+        message: '='
+    },
+    link: function (scope, element, attrs) {
+      
+      element.on('click', function (e) {
+        e.preventDefault();
+        Notify.alert(scope.message, scope.options);
+      });
+
+    }
+  };
+
+}]);
